75a68463431b8628fc05fb4b199a79c445be00da
Nikolaj Roager Christensen <nikolaj@debian.localhost>
2022-07-15 15:40:04 +0200

    stupid test of can anything be run on a computer which does not compile this thing

---
+++ stupid_test_1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//Just include filesystem, don't actually use it for anything
+#include<filesystem>
+
+
+int main(int argc, char* argv[])
+{
+
+    std::cout<<"THIS WILL NOT BE PRINTED, BECAUSE THIS CAN NOT RUN"<<std::endl;
+}
